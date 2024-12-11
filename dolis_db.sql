7231aa85f4d4e5662fd4f057e56a47833145f36a
aviennn <avienflairebatul@gmail.com>
2024-12-11 23:04:14 +0800

    added product

--- dolis_db.sql
+++ dolis_db.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 10, 2024 at 07:00 PM
+-- Generation Time: Dec 11, 2024 at 04:03 PM
 -- Server version: 10.4.28-MariaDB
 -- PHP Version: 8.2.4
 
@@ -45,7 +45,8 @@ INSERT INTO `products` (`id`, `product_name`, `price`, `image`, `description`, `
 (1, 'Carrot Cake', '900', 'carrot.png', 'A soft, spiced dessert made with grated carrots and topped with creamy cream cheese frosting. Sweet and flavorful, it\'s perfect for any occasion!', 'Wheat, Egg, Milk, Sulphites, Nuts', 'Chilled 2-3 days'),
 (2, 'Chocolate Cake', '900', 'chocolate.png', 'A rich and moist dessert made with cocoa, offering a deep and indulgent chocolate flavor. Often layered or topped with creamy chocolate frosting, it\'s a classic favorite for birthdays, parties, and special occasions. Perfect for chocolate lovers!\n\na rich ', 'Wheat (gluten), Eggs, Dairy (milk/butter), Cocoa', 'Chilled 3-4 days'),
 (3, 'Mango Cake', '900', 'mango.png', 'A light and fluffy dessert made with fresh mangoes, giving it a sweet and tropical flavor. Often layered with mango-flavored cream or frosting, it’s a refreshing treat perfect for any occasion!', 'Wheat/Gluten, Soy, Egg, Milk/Dairy, Tree Nuts', 'Chilled 3-5 days'),
-(4, 'Classic Chocolate Chip', '50', 'chocolatecookie.png', 'Classic Chocolate Chip is a timeless treat featuring soft, chewy cookies loaded with rich, melty chocolate chips—perfectly baked for a delightful balance of sweetness and comfort in every bite.', 'Wheat, Dairy, Eggs, Soy, Cocoa', 'Chilled 3-4 days');
+(4, 'Classic Chocolate Chip', '50', 'chocolatecookie.png', 'Classic Chocolate Chip is a timeless treat featuring soft, chewy cookies loaded with rich, melty chocolate chips—perfectly baked for a delightful balance of sweetness and comfort in every bite.', 'Wheat, Dairy, Eggs, Soy, Cocoa', 'Chilled 3-4 days'),
+(5, 'Carrot Cupcake', '160/box', 'carrotcupcake.png', 'A delightful box of five, a classic favorite, featuring a moist and fluffy carrot-infused base with a hint of warm spices, crowned with a swirl of velvety cream cheese frosting. Perfect for an on-the-go treat!', 'Dairy, Eggs, Wheat, Nuts', 'Chilled 3-5 days');
 
 --
 -- Indexes for dumped tables
