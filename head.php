b6a2a1d86c6b689f90eb5cebb95f4300f263663d
Paul Trustan S. Yumang <127754476+paulyumang@users.noreply.github.com>
2024-12-13 04:31:53 +0800

    Adjust Google Tag

--- head.php
+++ head.php
@@ -18,7 +18,7 @@ $pageKeywords = isset($pageKeywords) ? $pageKeywords : 'cakes, pastries, bakery,
     <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
     new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
     j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-    'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+    '//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
     })(window,document,'script','dataLayer','GTM-PBGNBW8K');</script>
     <!-- End Google Tag Manager -->
 
